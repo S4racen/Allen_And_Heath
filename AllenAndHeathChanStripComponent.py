23c8e30f6a2300db01470b1db7062891303c996b
S4racen <darrenecowley@sky.com>
2013-07-08 18:33:06 +0100

    Update and rename LividChanStripComponent.py to AllenAndHeathChanStripComponent.py

--- AllenAndHeathChanStripComponent.py
+++ AllenAndHeathChanStripComponent.py
@@ -1,6 +1,6 @@
 from _Framework.ChannelStripComponent import ChannelStripComponent
 
-class LividChanStripComponent(ChannelStripComponent):
+class AllenAndHeathChanStripComponent(ChannelStripComponent):
   ' Customized chan strip which limits volume to 0db '
 
   def _select_value(self, value):
